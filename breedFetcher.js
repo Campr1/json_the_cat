ff9f51b68ca4d6e6f461d13c0824879963d75524
Rishard C <rishard.c@hotmail.com>
2021-12-02 18:33:03 -0500

    refactorwith mocha and chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,20 @@
-const request = require('request');
-const args = process.argv.slice(2);
+const request = require("request");
+// const args = process.argv.slice(2);
 
-let breed = args[0];
+const fetchBreedDescription = function(breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null);
+      return;
+    }
+    if (error) {
+      console.log("API Error", error.message);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-
-  
-  const data = JSON.parse(body);
- 
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription };
